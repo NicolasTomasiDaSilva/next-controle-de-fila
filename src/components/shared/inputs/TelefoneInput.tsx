50e9e7ac720fbbecf3e0adf3534bae58272a09c4
Nicolas Tomasi <tomasitrabalho@gmail.com>
2025-05-14 23:11:38 -0300

    melhorando telefone input para funcionar com 10 digitos

--- src/components/shared/inputs/TelefoneInput.tsx
+++ src/components/shared/inputs/TelefoneInput.tsx
@@ -15,11 +15,17 @@ export default function TelefoneInput({
       <Label htmlFor="telefone">Telefone</Label>
       <IMaskInput
         id="telefone"
-        mask="(00) 00000-0000"
-        unmask={true}
         value={telefone}
+        unmask={true}
         onAccept={(value: string) => setTelefone(value)}
         placeholder="(xx) xxxxx-xxxx"
+        mask={[{ mask: "(00) 0000-0000" }, { mask: "(00) 00000-0000" }]}
+        dispatch={(appended, dynamicMasked) => {
+          const number = (dynamicMasked.value + appended).replace(/\D/g, "");
+          return number.length > 10
+            ? dynamicMasked.compiledMasks[1]
+            : dynamicMasked.compiledMasks[0];
+        }}
         className="w-full rounded-md border border-gray-300 px-3 py-2 text-sm
                    shadow-sm placeholder:text-muted-foreground
                    focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2
