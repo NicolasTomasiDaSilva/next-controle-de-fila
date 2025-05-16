b4a764719c11ed11cdae7619301df996b5a4468d
Nicolas Tomasi <tomasitrabalho@gmail.com>
2025-05-16 08:56:31 -0300

    colocando funcoes da fila dentro do hook

--- src/components/shared/ClienteRowTabela.tsx
+++ src/components/shared/ClienteRowTabela.tsx
@@ -17,9 +17,6 @@ import {
   XCircle,
 } from "lucide-react";
 import { StatusEnum, StatusMap } from "@/enums/status-enum";
-import { Button } from "../ui/button";
-import { filaService } from "@/services/fila-service-client";
-import { Fila } from "@/models/fila";
 import { useFila } from "@/hooks/use-fila";
 import { EditarClienteDialog } from "../TabelaPrincipal/EditarClienteDialog";
 
@@ -30,65 +27,19 @@ interface RowClientePersonalizadaProps {
 export default function ClienteRowTable({
   cliente,
 }: RowClientePersonalizadaProps) {
-  const { fila, setFila } = useFila();
-
-  async function onChamar() {
-    const filaAtualizada: Fila = await filaService.ChamarCliente(cliente.id);
-    setFila(filaAtualizada);
-  }
-  async function onRemover() {
-    const filaAtualizada: Fila = await filaService.RemoverCliente(cliente.id);
-    setFila(filaAtualizada);
-  }
-  async function onAusentar() {
-    const filaAtualizada: Fila = await filaService.AusentarCliente(cliente.id);
-    setFila(filaAtualizada);
-  }
-  async function onMoverCima() {
-    const novaPosicao: number = (cliente.posicao as number) - 1;
-    if (!isPosicaoValida(novaPosicao)) {
-      return;
-    }
-    const filaAtualizada: Fila = await filaService.MoverCliente(
-      cliente.id,
-      novaPosicao
-    );
-    setFila(filaAtualizada);
-  }
-  async function onMoverBaixo() {
-    const novaPosicao: number = (cliente.posicao as number) + 1;
-    if (!isPosicaoValida(novaPosicao)) {
-      return;
-    }
-    const filaAtualizada: Fila = await filaService.MoverCliente(
-      cliente.id,
-      novaPosicao
-    );
-    setFila(filaAtualizada);
-  }
-
-  async function onAtender() {
-    const filaAtualizada: Fila = await filaService.AtenderCliente(cliente.id);
-    setFila(filaAtualizada);
-  }
-
-  async function onVoltar() {
-    const filaAtualizada: Fila = await filaService.VoltarCliente(cliente.id);
-    setFila(filaAtualizada);
-  }
-
-  function isPosicaoValida(novaPosicao: number): boolean {
-    const isValida = fila.clientes.some(
-      (cliente) =>
-        cliente.status === StatusEnum.Aguardando &&
-        cliente?.posicao == novaPosicao
-    );
-    return isValida;
-  }
+  const {
+    onChamar,
+    onRemover,
+    onAusentar,
+    onMoverCima,
+    onMoverBaixo,
+    onAtender,
+    onVoltar,
+  } = useFila();
 
   return (
     <td>
-      <div className="w-full px-5 py-5 grid grid-cols-1 md:flex md:flex-row md:space-between md:grid-cols-2 md:gap-2">
+      <div className="w-full px-5 py-5 grid grid-cols-1 md:flex md:flex-row md:space-between md:gap-2">
         <div className="flex flex-row items-center gap-2 col-span-1">
           {cliente.posicao && (
             <span className="text-lg font-extrabold text-blue-500 mr-2 md:mr-6 ">
@@ -150,25 +101,33 @@ export default function ClienteRowTable({
           {cliente.status === StatusEnum.Aguardando && (
             <div className=" min-w-max w-[max-content] grid grid-cols-2 md:grid-cols-4 gap-2 ">
               <button
-                onClick={onMoverCima}
+                onClick={async () => {
+                  onMoverCima(cliente);
+                }}
                 className="text-black-600 hover:text-black-700 transition md:order-2 cursor-pointer"
               >
                 <CircleArrowUp className="w-7 h-7" />
               </button>
               <button
-                onClick={onMoverBaixo}
+                onClick={async () => {
+                  onMoverBaixo(cliente);
+                }}
                 className="text-black-600 hover:text-black-700 transition md:order-3 cursor-pointer"
               >
                 <CircleArrowDown className="w-7 h-7" />
               </button>
               <button
-                onClick={onChamar}
+                onClick={async () => {
+                  onChamar(cliente);
+                }}
                 className="text-green-600 hover:text-green-700 transition md:order-1 cursor-pointer"
               >
                 <Phone className="w-7 h-7" />
               </button>
               <button
-                onClick={onRemover}
+                onClick={async () => {
+                  onRemover(cliente);
+                }}
                 className="text-red-600 hover:text-red-700 transition md:order-4 cursor-pointer"
               >
                 <Trash className="w-7 h-7" />
@@ -178,20 +137,26 @@ export default function ClienteRowTable({
           {cliente.status === StatusEnum.Chamado && (
             <div className=" min-w-max w-[max-content] grid grid-cols-3 gap-2 ">
               <button
-                onClick={onAtender}
+                onClick={async () => {
+                  onAtender(cliente);
+                }}
                 className="text-green-500 transition md:order-1 cursor-pointer"
               >
                 <CheckCircle className="w-7 h-7" />
               </button>
 
               <button
-                onClick={onVoltar}
+                onClick={async () => {
+                  onVoltar(cliente);
+                }}
                 className="text-blue-500  transition md:order-4 cursor-pointer"
               >
                 <CircleArrowLeft className="w-7 h-7" />
               </button>
               <button
-                onClick={onAusentar}
+                onClick={async () => {
+                  onAusentar(cliente);
+                }}
                 className="text-red-500 transition md:order-1 cursor-pointer"
               >
                 <XCircle className="w-7 h-7" />
--- src/hooks/use-fila.tsx
+++ src/hooks/use-fila.tsx
@@ -1,4 +1,8 @@
 import { FilaContext } from "@/contexts/fila-context";
+import { StatusEnum } from "@/enums/status-enum";
+import { Cliente } from "@/models/cliente";
+import { Fila } from "@/models/fila";
+import { filaService } from "@/services/fila-service-client";
 import { useContext } from "react";
 
 export const useFila = () => {
@@ -6,5 +10,71 @@ export const useFila = () => {
   if (context === undefined) {
     throw new Error("useUser must be used within a UserProvider");
   }
-  return context;
+  const { fila, setFila } = context;
+
+  async function onChamar(cliente: Cliente) {
+    const filaAtualizada: Fila = await filaService.ChamarCliente(cliente.id);
+    setFila(filaAtualizada);
+  }
+  async function onRemover(cliente: Cliente) {
+    const filaAtualizada: Fila = await filaService.RemoverCliente(cliente.id);
+    setFila(filaAtualizada);
+  }
+  async function onAusentar(cliente: Cliente) {
+    const filaAtualizada: Fila = await filaService.AusentarCliente(cliente.id);
+    setFila(filaAtualizada);
+  }
+  async function onMoverCima(cliente: Cliente) {
+    const novaPosicao: number = (cliente.posicao as number) - 1;
+    if (!isPosicaoValida(novaPosicao)) {
+      return;
+    }
+    const filaAtualizada: Fila = await filaService.MoverCliente(
+      cliente.id,
+      novaPosicao
+    );
+    setFila(filaAtualizada);
+  }
+  async function onMoverBaixo(cliente: Cliente) {
+    const novaPosicao: number = (cliente.posicao as number) + 1;
+    if (!isPosicaoValida(novaPosicao)) {
+      return;
+    }
+    const filaAtualizada: Fila = await filaService.MoverCliente(
+      cliente.id,
+      novaPosicao
+    );
+    setFila(filaAtualizada);
+  }
+
+  async function onAtender(cliente: Cliente) {
+    const filaAtualizada: Fila = await filaService.AtenderCliente(cliente.id);
+    setFila(filaAtualizada);
+  }
+
+  async function onVoltar(cliente: Cliente) {
+    const filaAtualizada: Fila = await filaService.VoltarCliente(cliente.id);
+    setFila(filaAtualizada);
+  }
+
+  function isPosicaoValida(novaPosicao: number): boolean {
+    const isValida = fila.clientes.some(
+      (cliente) =>
+        cliente.status === StatusEnum.Aguardando &&
+        cliente?.posicao == novaPosicao
+    );
+    return isValida;
+  }
+
+  return {
+    fila,
+    setFila,
+    onChamar,
+    onRemover,
+    onAusentar,
+    onMoverCima,
+    onMoverBaixo,
+    onAtender,
+    onVoltar,
+  };
 };
