eaeeb6ee5b6ace3cb45a8d1e4cc276ae00d13901
Nicolas Tomasi <tomasitrabalho@gmail.com>
2025-05-17 18:25:42 -0300

    melhorias no input de telefone

--- src/components/shared/inputs/TelefoneInput.tsx
+++ src/components/shared/inputs/TelefoneInput.tsx
@@ -1,4 +1,5 @@
 import { Label } from "@/components/ui/label";
+import { cn } from "@/lib/utils";
 import { IMaskInput } from "react-imask";
 
 interface TelefoneInputProps {
@@ -28,10 +29,9 @@ export default function TelefoneInput({
             ? dynamicMasked.compiledMasks[1]
             : dynamicMasked.compiledMasks[0];
         }}
-        className="w-full rounded-md border border-gray-300 px-3 py-2 text-sm
-                   shadow-sm placeholder:text-muted-foreground
-                   focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2
-                   disabled:cursor-not-allowed disabled:opacity-50"
+        className={
+          "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive"
+        }
         type="tel"
       />
       {error && <p className="text-red-500 text-sm">{error}</p>}
