11990e585b29948d2f2035aa406d08d94505f44c
Nicolas Tomasi <tomasitrabalho@gmail.com>
2025-05-23 00:06:54 -0300

    sinc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,9 +24,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning={true}
       >
         {children}
         <Toaster
