f6d3a858ec3afa8c802292686d7badb30852de48
Nicolas Tomasi <tomasitrabalho@gmail.com>
2025-05-30 16:58:16 -0300

    sinc

--- src/components/pages/vinculacao/vinculacao-content.tsx
+++ src/components/pages/vinculacao/vinculacao-content.tsx
@@ -3,7 +3,7 @@ import CardVincularVinculacao from "./card-vincular-vinculacao";
 
 export default function VinculacaoContent() {
   return (
-    <div>
+    <div className="flex flex-col gap-4 md:flex-row">
       <CardInstrucoesVinculacao></CardInstrucoesVinculacao>
       <CardVincularVinculacao></CardVincularVinculacao>
     </div>
