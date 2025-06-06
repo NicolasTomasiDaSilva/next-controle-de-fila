63d65c7248d36c781deed79786fb594b7ec802ca
Nicolas Tomasi <tomasitrabalho@gmail.com>
2025-06-06 16:54:23 -0300

    sinc

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["avatars.githubusercontent.com"],
   },
-  allowedDevOrigins: [
-    "http://10.0.0.65:3000",
-    "https://8718-45-191-38-22.ngrok-free.app",
-    "https://36d4-45-191-38-22.ngrok-free.app",
-  ],
+  allowedDevOrigins: ["http://10.0.0.65:3000"],
   reactStrictMode: false,
   logging: {
     fetches: {
