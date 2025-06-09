828a4780b9d727195a75fc26405375a060ce5cf6
Nicolas Tomasi <tomasitrabalho@gmail.com>
2025-06-09 08:22:12 -0300

    sinc

--- src/constantes/mensagens-padrao-whatsapp.ts
+++ src/constantes/mensagens-padrao-whatsapp.ts
@@ -1,7 +1,7 @@
 export const mensagensPadraoWhatsapp = {
   mensagemEntrada:
-    "Olá, {nome}! Você foi adicionado(a) com sucesso à nossa fila de atendimento. Para acompanhar sua posição em tempo real, acesse o link: {link}. Agradecemos pela sua paciência!",
+    "Olá, {nome}! Você foi adicionado(a) com sucesso à nossa fila de atendimento. Para acompanhar sua posição em tempo real, acesse o link: {link}.",
   mensagemChamada:
-    "Olá, {nome}! Chegou a sua vez! Estamos prontos para atendê-lo(a). Por favor, dirija-se ao local indicado. Caso precise verificar detalhes, acesse: {link}.",
+    "Olá, {nome}! Chegou a sua vez! Estamos prontos para atendê-lo(a).",
   mensagemRemovido: "Olá, {nome}! Você foi removido(a) da fila.",
 };
