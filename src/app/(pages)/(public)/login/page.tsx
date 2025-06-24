94834c91c79fe4c798084c957b82f9f35339bf85
Nicolas Tomasi <tomasitrabalho@gmail.com>
2025-06-24 11:18:44 -0300

    sinc

--- src/app/(pages)/(public)/login/page.tsx
+++ src/app/(pages)/(public)/login/page.tsx
@@ -1,5 +1,6 @@
 import LoginForm from "@/features/autenticacao/components/login-form";
 
+export const dynamic = "force-dynamic";
 export default async function LoginPage() {
   return <LoginForm></LoginForm>;
 }
